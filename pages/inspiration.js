60e50ba2835c70df672acf9b6910d27e11061cc0
isaiahL88 <isaiah.ib@outlook.com>
2021-05-10 23:58:36 -0400

    Added more to inspiration Page

---
+++ pages/inspiration.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const inspiration = () => {
+    return (
+        <div>
+            <h3>Inspiration</h3>
+            <p>My Inspiration as a Hairstylist and a Service Business Owner or Principal is Joy I can spread by giving quality services to the public</p>
+        </div>
+    )
+}
+
+export default inspiration
